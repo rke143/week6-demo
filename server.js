529c758ebc51d5e922df5b669b6e1bc386a08085
Julia Vrn <julia.voronetskaja@tktk.ee>
2023-10-10 11:46:49 +0200

    week 6 files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = 3000;
+const booksRouter = require('./routes/books');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {    
+    res.render('index');
+});
+
+app.use('/books', booksRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
